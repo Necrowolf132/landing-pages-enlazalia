e4302f269c6b7c2f0b587ade4123f90378aeb6a1
Necrowolf132 <manjou132@gmail.com>
2019-04-23 22:57:16 -0400

    añadido gulp y archivo compilado .zip

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+// VARIABLES
+const concat        = require('gulp-concat');
+//const sass          = require('gulp-sass');
+const postcss       = require('gulp-postcss');
+const autoprefixer  = require('autoprefixer');
+const flexboxfixer  = require('postcss-flexboxfixer');
+const uglify        = require('gulp-uglify');
+const minifycss     = require('gulp-minify-css');
+const browserSync   = require('browser-sync').create();
+const gulp          = require('gulp');
+const zip           = require('gulp-zip');
+
+// FILES
+gulp.task('files', function() {
+    var fonts = gulp.src('assets/css/fonts/*-*')
+    .pipe(gulp.dest('assetbuild/fonts'));
+    var img = gulp.src('assets/css/img/*.*')
+        .pipe(gulp.dest('assetbuild/img'));
+    var webfonts = gulp.src('assets/css/webfonts/*.*')
+        .pipe(gulp.dest('assetbuild/webfonts'));
+    return (fonts, img, webfonts);
+});
+
+// CSS
+gulp.task('css', function() {
+    return gulp.src([
+        'assets/css/slick.css',
+        'assets/css/bootstrap.css',
+        'assets/css/style.css',
+        'assets/css/all.min.css'
+    ]).pipe(concat('todo.min.css'))
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(postcss([ flexboxfixer() ]))
+        .pipe(minifycss())
+        .pipe(gulp.dest('assetbuild'))
+        .pipe(browserSync.stream());
+});
+
+// JS
+gulp.task('js', function() {
+    return gulp.src([
+        'assets/js/jquery.min.js',
+        'assets/js/migrate.min.js',
+        'assets/js/popper.js',
+        'bootstrap/js/bootstrap.min.js',
+        'assets/slick/velocity.min.js',
+        'assets/slick/slider3d.js',
+        'assets/js/mi_script.js'
+        ])
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assetbuild'))
+        .pipe(browserSync.stream());
+});
+
+// SERVE
+gulp.task('serve', function() {
+    exports.browserSync = browserSync.init({
+        // server: './'' // default server
+        // proxy: 'http://localhost:8888/' // mamp
+        // la localhost de la instalacion de jommla
+        proxy: 'http://localhost/archivos%20de%20maquetado%20/Lainding%20Page%20español'
+        // usualy
+    });
+    gulp.watch('**/*.css', gulp.series('zip')).on('change', browserSync.reload);
+    gulp.watch('**/*.js', gulp.series('zip')).on('change', browserSync.reload);
+    gulp.watch('**/*.html', gulp.series('zip')).on('change', browserSync.reload);
+});
+
+// ZIP
+gulp.task('zip', function() {
+    return gulp.src([
+        '**/*.*',
+        '!_pgbackup/**',
+        '!bootstrap/**',
+        '!assets/**',   
+        '!./LaindingFinal.zip',
+        '!.git/**',
+        '!js/**',
+        '!Landing page_ES (1).PDF',
+        '!pinegrow.json',
+        '!style.css',
+        '!gulpfile.js'
+    ])
+        .pipe(zip('LaindingFinal.zip'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', gulp.series('files','css','js','zip','serve'));
+
